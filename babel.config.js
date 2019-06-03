43b6e9a45f274277756dcfeb831aa737a5fd0638
tsejx <tsejx@foxmail.com>
2019-06-03 23:34:15 +0800

    :wrench: Update babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['module-resolver', {
+      'root': ['./src'],
+      'alias': {
+        'base': './src/base',
+        'pages': './src/pages',
+        'navigator': './src/navigator',
+        'constants': './src/constants',
+        'utils': './src/utils',
+      }
+    }]
+  ]
 };
